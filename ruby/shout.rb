d27e063f0b83f50b70300ffe42b02fbacd00178e
John Winslow <winslow.johnm@gmail.com>
2016-10-02 21:42:35 -0700

    create Shout module, add driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,14 @@
 module Shout
-	# code to follow
+
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  	words.upcase + "!!!" + " :)"
+  end
+
 end
+
+puts Shout.yell_happily("woohooo")
+puts Shout.yell_angrily("awww skeet skeet gawd damn")
