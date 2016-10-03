ca38dfe3c811b1f205a4d771d074d751fc5c5e84
John Winslow <winslow.johnm@gmail.com>
2016-10-03 15:26:58 -0700

    Add initial pseudocode to explore.js

---
+++ js/explore.js
@@ -0,0 +1,27 @@
+// declare variable
+
+var str = "reverse this string!";
+
+for (var i = 0; i < str.length; i++) {
+
+}
+// break string apart into characters
+// reverse order of characters
+// put back together in single string
+
+
+// var str = "Loop over me!";
+
+// Look at line 8:
+// We initialize our counter, set its "while" condition, 
+// and give the increment command that should happen 
+// after each iteration of the loop -- 
+// all in one tight line of code!
+
+// for (var i = 0; i < str.length; i++) {
+//	console.log(str[i]);
+// }
+
+// for (var i = 10; i >= 0; i--) {
+//	console.log(i)
+// }
