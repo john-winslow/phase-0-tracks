95f8d4c692ae421f74ada8f8123145d0ee73407d
John Winslow <winslow.johnm@gmail.com>
2016-10-03 15:46:18 -0700

    Create reverse word function

--- js/explore.js
+++ js/explore.js
@@ -3,29 +3,14 @@ function reverse(str) {
 
 // create an empty string to populate 
 	var reversed = "";
-// for loop: start at end of string (-1)
-	for (var i = str.length; i > 0; i--) {
-		reversed =+ str[i];
+// for loop: start at end of string (-1), loop over each
+// letter and add to new string, until i reaches 0
+	for (var i = str.length - 1; i >= 0; i--) {
+		reversed += str[i];
 	}
-
+	console.log(reversed);
 }
-// break string apart into characters
-// reverse order of characters
-// put back together in single string
 
-
-// var str = "Loop over me!";
-
-// Look at line 8:
-// We initialize our counter, set its "while" condition, 
-// and give the increment command that should happen 
-// after each iteration of the loop -- 
-// all in one tight line of code!
-
-// for (var i = 0; i < str.length; i++) {
-//	console.log(str[i]);
-// }
-
-// for (var i = 10; i >= 0; i--) {
-//	console.log(i)
-// }
+// test
+reverse("hello");
+reverse("namaste")
