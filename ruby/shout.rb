fcc6c7941ff7f9084b6e4129832e5dddfa179749
John Winslow <winslow.johnm@gmail.com>
2016-10-02 21:55:35 -0700

    Add driver code for mixin modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,44 @@
+# module Shout
+#  def self.yell_angrily(words)
+#   words + "!!!" + " :("
+#  end
+
+#  def self.yell_happily(words)
+#  	words.upcase + "!!!" + " :)"
+#  end
+
+# end
+
+# DRIVER CODE (test):
+
+# puts Shout.yell_happily("woohooo")
+# puts Shout.yell_angrily("awww skeet skeet gawd damn")
+
+
+# Release 3: Standalone --> Mixin module
 module Shout
+	def yell_angrily(words)
+		p words + "!!!" + " :("
+	end
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
-
-  def self.yell_happily(words)
-  	words.upcase + "!!!" + " :)"
-  end
-
+	def yell_happily(words)
+		p words.upcase + "!!!" +" :)"
+	end
 end
 
-puts Shout.yell_happily("woohooo")
-puts Shout.yell_angrily("awww skeet skeet gawd damn")
+class Woman
+	include Shout
+end
+
+class Man
+	include Shout
+end
+
+# DRIVER CODE:
+
+woman = Woman.new
+woman.yell_happily("hello world")
+
+man = Man.new
+man.yell_angrily("Where are my slippers?")
+
