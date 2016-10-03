0e81c3fd625b7c8bf4a2a44488cb951b217ba5df
John Winslow <winslow.johnm@gmail.com>
2016-10-02 23:59:43 -0700

    Add initialize method to game class

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,7 +1,34 @@
 class WordGuess
 
-	def initialize
+	attr_accessor :word, :guess_number, :updated_word
 
+	def initialize(word)
+		@word = word
+		@guess_number = @word.length + 1
+		@guessed_letters = []
+		@updated_word = @word.length * "_"
 	end
 
+	
+
 end
+
+
+
+
+# INPUTS:
+# => first word
+# => 
+
+
+
+# One user enters a phrase, other user tries to guess it
+
+# guesses are limited, relative to length of the word
+
+# repeated guesses do not count against the user
+
+# guessing player receives continual feedback on the current state of the word..
+# "_______" -> "___c___" if word is 'unicorn' and user has guessed C
+
+# congratulatory message if they win, taunting if they lose
