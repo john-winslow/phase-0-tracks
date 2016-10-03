e7d1e87c567aec0dcc15e9ba84dd2108e72ab98e
John Winslow <winslow.johnm@gmail.com>
2016-10-03 01:12:32 -0700

    Add WordGuess class & methods, update rspect test file

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,34 +1,72 @@
 class WordGuess
 
-	attr_accessor :word, :guess_number, :updated_word
+	attr_accessor :word, :guess_counter, :current_word
 
 	def initialize(word)
 		@word = word
-		@guess_number = @word.length + 1
+		@current_word = "_" * @word.length 
 		@guessed_letters = []
-		@updated_word = @word.length * "_"
+		@guess_counter = @word.length + 2
 	end
 
-	
+	def guess(letter)
+		if @guessed_letters.include?(letter)
+			nil
+		else
+			@guess_counter -= 1
+			@guessed_letters << letter
+			if @word.include?(letter)
+				update(letter)
+				true
+			else
+				false
+			end
+		end
+	end
 
+	def update(letter)
+		counter = 0
+		@current_word_array = @current_word.split("")
+		@new_word_array = []
+		until counter == @word.length
+			@current_word_array.each do |char|
+			if letter == @word[counter]
+				@new_word_array << letter
+			else
+				@new_word_array << char
+			end
+			counter += 1
+		end
+		@current_word = @new_word_array.join("")
+	end
+
+	def game_over
+		if @current_word == @word
+			puts "Congratulation!  You won the game."
+		elsif @guess_counter == 0
+			puts "You lost!  All you had to do was guess #{@word}!"
+		end
+	end
+
+end
+
+puts "Welcome to the WordGuess game!  Enter a word for player2 to guess:"
+game1 = WordGuess.new(gets.chomp)
+
+until game1.word == game1.current_word || game1.guess_counter == 0
+	puts "Guess a letter - #{game1.guess_counter} guesses remaining."
+	letter = gets.chomp
+	input = game1.guess(letter)
+		if input == nil
+			puts "Letter already guessed.  Pick another!"
+		elsif input == true
+			puts "Well done - there is a #{letter} in the secret word."
+			puts "#{game1.current_word}"
+		elsif input == false
+			puts "Fail!  There are no #{letter}'s in the secret word.  Guess again!"
+		end
+end
 end
 
 
 
-
-# INPUTS:
-# => first word
-# => 
-
-
-
-# One user enters a phrase, other user tries to guess it
-
-# guesses are limited, relative to length of the word
-
-# repeated guesses do not count against the user
-
-# guessing player receives continual feedback on the current state of the word..
-# "_______" -> "___c___" if word is 'unicorn' and user has guessed C
-
-# congratulatory message if they win, taunting if they lose
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,1 +1,18 @@
 require_relative "game"
+
+describe WordGuess do
+	let(:game) { WordGuess.new("hello") }	
+
+	it "stores input word" do
+		expect(game.word).to eq "hello"
+	end
+
+	it "sets guess count as 2 more than length of input word" do
+		expect(game.guess_counter).to eq 6
+	end
+
+	it "creates a string of underscores the same length as input word" do
+		expect(game.current_word).to eq "____"
+	end
+end
+
