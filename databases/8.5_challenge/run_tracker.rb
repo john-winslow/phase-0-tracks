a25cf95617f86923bb6578657300506f7ece87ed
John Winslow <winslow.johnm@gmail.com>
2016-10-21 17:20:19 -0700

    Add driver code and pseudocode

--- databases/8.5_challenge/run_tracker.rb
+++ databases/8.5_challenge/run_tracker.rb
@@ -1,7 +1,9 @@
 require 'sqlite3'
 
+# create new database file logging runs
 db = SQLite3::Database.new("log.db")
 
+# SQL string to create new table 
 create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS runs(
     id INTEGER PRIMARY KEY,
@@ -10,4 +12,29 @@ create_table_cmd = <<-SQL
   )
 SQL
 
+# create run table within log database
 db.execute(create_table_cmd)
+
+# method to add time and duration to run database
+def add_run(db, distance, duration)
+  db.execute("INSERT INTO runs (distance, duration) VALUES (?, ?)", [distance, duration])
+end
+
+# method to return total miles logged
+
+# method to return total duration logged
+
+# method to ruturn average pace logged
+
+
+# DRIVER CODE
+
+
+puts "Would you like to add a run? (y/n)"
+
+
+puts "------------------------"
+puts "Total miles: "
+puts "Total time: "
+puts "Average pace: "
+puts "------------------------"
