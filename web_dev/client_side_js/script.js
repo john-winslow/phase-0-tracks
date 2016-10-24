39b7bff59305253df020915e637905842f701271
John Winslow <winslow.johnm@gmail.com>
2016-10-23 20:30:56 -0700

    Initial js manipulations

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,9 @@
+// print text to test that script is loading
+console.log("Initiated!");
+
+// define picture variable
+var pic = document.getElementById("celebrate");
+
+// add border to picture element
+pic.style.border = "5px solid black";
+
